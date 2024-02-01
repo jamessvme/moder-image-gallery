bc22e0463fe6fb7a0defebd8d20f4044142efe09
BorisMunro <munroboris@gmail.com>
2024-02-02 00:51:00 +0800

    fix: added ionic button

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,5 +1,5 @@
 import './ExploreContainer.css';
-
+import { IonButton } from '@ionic/react';
 interface ContainerProps {
   name: string;
 }
@@ -8,7 +8,18 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Explore 
+        <a 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
+
+      <IonButton color={"custom"}>Custom Button</IonButton>
     </div>
   );
 };
